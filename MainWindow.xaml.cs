03caf42b87785f028a9e268d550a4a2936575c91
avatar <avatar@arkane-systems.net>
2013-08-24 03:07:39 -0500

    Untested first cut.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,166 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region header
+
+// Rargh - MainWindow.xaml.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2013.
+// 
+// Created: 2013-07-21 6:53 PM
+
+#endregion
+
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using ArkaneSystems.Rargh;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Rargh
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string FolderName;
+
         public MainWindow ()
         {
-            InitializeComponent ();
+            this.InitializeComponent ();
+
+            this.Rarchives = new ObservableCollection<Rarchive> ();
+        }
+
+        public ObservableCollection<Rarchive> Rarchives { get; set; }
+
+        private void Window_Loaded (object sender, RoutedEventArgs e)
+        {
+            redofromstart:
+
+            // Window is loaded.  Now get the folder containing the rarchives.
+            var dlg = new CommonOpenFileDialog ();
+
+            dlg.Title = "Select folder containing the rarchives";
+            dlg.IsFolderPicker = true;
+            dlg.InitialDirectory = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems = false;
+            dlg.DefaultDirectory = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+            dlg.EnsureFileExists = true;
+            dlg.EnsurePathExists = true;
+            dlg.EnsureReadOnly = false;
+            dlg.EnsureValidNames = true;
+            dlg.Multiselect = false;
+            dlg.ShowPlacesList = true;
+
+            // Close the app if we cancel at this point.
+            if (dlg.ShowDialog (this) != CommonFileDialogResult.Ok)
+                this.Close ();
+
+            // Get the .rar files in the folder in question.
+            this.FolderName = dlg.FileName;
+
+            var rarchiveFiles = Directory.EnumerateFiles (this.FolderName, "*.rar", SearchOption.TopDirectoryOnly);
+
+            if (!rarchiveFiles.Any ())
+            {
+                MessageBox.Show (this,
+                                 "There are no .rarchives in that folder.",
+                                 "Rargh",
+                                 MessageBoxButton.OK,
+                                 MessageBoxImage.Warning);
+                goto redofromstart;
+            }
+
+            var oldCursor = Mouse.OverrideCursor;
+
+            try
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+
+                foreach (string s in rarchiveFiles)
+                    this.Rarchives.Add (new Rarchive (s));
+
+                this.Progress.Maximum = this.Rarchives.Count;
+            }
+            finally
+            {
+                Mouse.OverrideCursor = oldCursor;
+            }
+
+            // Now we've got the rarchives, enable the go button.
+            this.Go.IsEnabled = true;
+        }
+
+        private async void Go_Click (object sender, RoutedEventArgs e)
+        {
+            // Re-disable the button.
+            this.Go.IsEnabled = false;
+
+            // Trigger the async method which does the actual work for each and every one on the thread pool.
+            await Task.Run (async () => await this.GoAsync ());
+        }
+
+        private async Task GoAsync ()
+        {
+            foreach (Rarchive rar in this.Rarchives)
+                await rar.Process (this);
+        }
+
+        public void StatusIsStarting (Rarchive rar)
+        {
+            this.Dispatcher.Invoke (() =>
+                                    {
+                                        this.Output.Items.Add (string.Format ("Starting: {0}", rar.Name));
+
+                                        // Make sure Output is scrolled to the bottom.
+                                        this.Output.ScrollIntoView (this.Output.Items[this.Output.Items.Count - 1]);
+                                    });
+        }
+
+        public void StatusIsExtracted (Rarchive rar, string file)
+        {
+            this.Dispatcher.Invoke (() =>
+                                    {
+                                        this.Output.Items.Add (string.Format ("...extracted: {0}", file));
+
+                                        // Make sure Output is scrolled to the bottom.
+                                        this.Output.ScrollIntoView (this.Output.Items[this.Output.Items.Count - 1]);
+                                    });
+        }
+
+        public void StatusIsDone (Rarchive rar)
+        {
+            this.Dispatcher.Invoke (() =>
+                                    {
+                                        this.Output.Items.Add (string.Format ("Finished: {0}", rar.Name));
+                                        this.Progress.Value++;
+
+                                        // Make sure Output is scrolled to the bottom.
+                                        this.Output.ScrollIntoView (this.Output.Items[this.Output.Items.Count - 1]);
+                                    });
+        }
+
+        public void StatusIsError (Rarchive rar, string error)
+        {
+            this.Dispatcher.Invoke (() =>
+                                    {
+                                        this.Output.Items.Add (string.Format ("Error processing file: {0} - {1}",
+                                                                              rar.Name,
+                                                                              error));
+                                        this.Progress.Value++;
+
+                                        // Make sure Output is scrolled to the bottom.
+                                        this.Output.ScrollIntoView (this.Output.Items[this.Output.Items.Count - 1]);
+                                    });
         }
     }
 }
---
+++ Rarchive.cs
@@ -0,0 +1,93 @@
+﻿#region header
+
+// Rargh - Rarchive.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2013.
+// 
+// Created: 2013-08-24 1:53 AM
+
+#endregion
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+using Rargh;
+
+using SharpCompress.Archive;
+
+namespace ArkaneSystems.Rargh
+{
+    /// <summary>
+    ///     Internal representation of a rarchive.
+    /// </summary>
+    public class Rarchive
+    {
+        public Rarchive (string path)
+        {
+            this.PathName = path;
+            this.Color = Colors.Black;
+        }
+
+        public string PathName { get; set; }
+
+        public Color Color { get; set; }
+
+        public string Name
+        {
+            get { return Path.GetFileNameWithoutExtension (this.PathName); }
+        }
+
+        /// <summary>
+        ///     Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        ///     A string that represents the current object.
+        /// </returns>
+        public override string ToString ()
+        {
+            return this.Name;
+        }
+
+        public async Task Process (MainWindow main)
+        {
+            main.StatusIsStarting (this);
+
+            try
+            {
+                // Get the stuff in the archive.
+                using (var archive = ArchiveFactory.Open (this.PathName))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        if (!entry.IsDirectory)
+                        {
+                            entry.WriteToDirectory (main.FolderName);
+                            main.StatusIsExtracted (this, entry.FilePath);
+
+                            await Task.Yield ();
+                        }
+                    }
+                }
+
+                File.Delete (this.PathName);
+            }
+            catch (Exception ex)
+            {
+                // We don't stop for anything.
+                main.StatusIsError (this, ex.Message);
+                this.Color = Colors.Red;
+
+                // And we quit hencewith.
+                return;
+            }
+
+            main.StatusIsDone (this);
+            this.Color = Colors.Gray;
+        }
+    }
+}
